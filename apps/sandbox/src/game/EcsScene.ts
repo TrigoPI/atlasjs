d9359a81087ea7f179a8e3f14de0dd16a7686949
TrigoPI <payetalexisg@orange.fr>
2026-07-16 14:58:34 +0400

    feat(gameplay): refactor EcsScene to use Sprite instances and improve texture loading

--- apps/sandbox/src/game/EcsScene.ts
+++ apps/sandbox/src/game/EcsScene.ts
@@ -6,8 +6,9 @@ import { type Entity, type NexusWorld, NEXUS } from "@atlasjs/nexus";
 import { TestScript } from "./scripts/TestScript";
 
 import {
-  ScriptManager,
+  type ScriptManager,
   SCRIPT_MANAGER,
+  Sprite,
   SpriteRender,
   Transform2D,
 } from "@atlasjs/gameplay";
@@ -28,38 +29,40 @@ export class EcsScene extends Scene {
     const nebula: NebulaRenderer = ctx.services.get(NEBULA_RENDERER);
     const scriptManager: ScriptManager = ctx.services.get(SCRIPT_MANAGER);
 
-    const swordImage: ImageBitmap = await this.getImage(SwordImage);
-    const swordTexture: Texture2D = nebula.createTexture2D({
-      source: swordImage,
-      height: swordImage.height,
-      width: swordImage.width,
-    });
+    const swordTexture: Texture2D = await this.loadTexture(nebula, SwordImage);
+    const seaLionTexture: Texture2D = await this.loadTexture(nebula, SeaLionImage);
 
-    const seaLionImage: ImageBitmap = await this.getImage(SeaLionImage);
-    const seaLionTexture: Texture2D = nebula.createTexture2D({
-      source: seaLionImage,
-      height: seaLionImage.height,
-      width: seaLionImage.width,
-    });
+    const swordSprite: Sprite = new Sprite(swordTexture);
+    const seaLionSprite: Sprite = new Sprite(seaLionTexture);
 
     const player1: Entity = nexus.createEntity();
     const player2: Entity = nexus.createEntity();
 
-    nexus.addComponent(player1, SpriteRender, swordTexture);
+    nexus.addComponent(player1, Transform2D);
+    nexus.addComponent(player1, SpriteRender, swordSprite);
 
-    nexus.addComponent(player2, SpriteRender, seaLionTexture);
     nexus.addComponent(player2, Transform2D);
+    nexus.addComponent(player2, SpriteRender, seaLionSprite);
 
     scriptManager.attach(player1, TestScript);
-    // scriptManager.attach(player2, TestScript);
   }
 
-  private async getImage(src: string): Promise<ImageBitmap> {
+  private async loadTexture(
+    nebula: NebulaRenderer,
+    src: string,
+  ): Promise<Texture2D> {
     const image: HTMLImageElement = new Image();
     image.src = src;
     await image.decode();
-    return createImageBitmap(image, {
+
+    const source: ImageBitmap = await createImageBitmap(image, {
       imageOrientation: "flipY",
+    });
+
+    return nebula.createTexture2D({
+      source,
+      width: source.width,
+      height: source.height,
     });
   }
 }
